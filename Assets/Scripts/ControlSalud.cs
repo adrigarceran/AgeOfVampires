251a9a9451f389434dab5443e3f607e0b12e0d5c
casti11 <castillogarciaguillermo@gmail.com>
2018-04-26 18:28:03 +0200

    Sangre y has muerto

--- Assets/Scripts/ControlSalud.cs
+++ Assets/Scripts/ControlSalud.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ControlSalud : MonoBehaviour {
 	int salud = 3;
 	float lastHit = 0.0f;
+	[SerializeField]
+	private GameObject salud2, salud1, muerto;
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +15,22 @@ public class ControlSalud : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (salud == 0) {
+		if (salud == 2)
+			salud2.SetActive (true);
+		else if (salud == 1) {
+			salud2.SetActive (false);
+			salud1.SetActive (true);
+		} else if (salud == 0) {
+			salud1.SetActive (false);
+			muerto.SetActive (true);
+			// Parar juego
+
+			// Capturar espacio y reiniciar
+
+			// Poner cámara en blanco y negro?
+
+			// Desactivar cuando se muera el moñeco
+
 			Debug.LogWarning ("MUERTO");
 		}
 		
