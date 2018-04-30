62f3b99f264f03b4158f9887579cea8d16da3925
casti11 <castillogarciaguillermo@gmail.com>
2018-04-30 21:15:19 +0200

    Canvas corregidos

--- Assets/Scripts/controlFinal.cs
+++ Assets/Scripts/controlFinal.cs
@@ -14,6 +14,7 @@ public class controlFinal : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Space)) {
+			control.defaultValues ();
 			SceneManager.LoadScene ("EntradaCastillo");	
 		}
 
